e8af2f1c8231c7d62040e21aeb510ba3b8cb0a9d
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-09-23 18:23:23 -0300

    Exemplo de Aplicação

---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import { FormEvent, useState } from "react"
+import { SearchResults } from "../components/SearchResults";
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault()
+
+    if (!search.trim()){
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`)
+    const data = await response.json()
+
+    setResults(data)
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1>Search</h1>
+
+      <form
+        onSubmit={handleSearch}
+      >
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
+    </div>
+  )
+}
